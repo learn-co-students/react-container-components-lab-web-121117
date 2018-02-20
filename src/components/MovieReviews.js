89180fea4ac4586dd9f6c5ea7f83821cd968683a
github username <github email address>
2018-02-20 17:56:37 -0500

    Done.

--- src/components/MovieReviews.js
+++ src/components/MovieReviews.js
@@ -1,1 +1,11 @@
-// Code MovieReviews Here
+import React from 'react'
+
+export default class MovieReviews extends React.Component {
+  render() {
+    return (
+      <div className="review-list">
+
+      </div>
+    )
+  }
+}
